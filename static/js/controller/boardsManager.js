7caad222f8a2e09e87c165481759d77ab73a6449
fergencszeno <fergencszeno@gmail.com>
2022-05-18 19:03:54 +0200

    implement toggle Add new column feature

--- static/js/controller/boardsManager.js
+++ static/js/controller/boardsManager.js
@@ -28,12 +28,30 @@ export function showHideButtonHandler(clickEvent) {
     const boardId = clickEvent.target.parentElement.dataset.boardId;
     const boardBody = document.querySelector(`.board-columns-${boardId}`);
     if (boardBody.style.display === "none"){
+        addColumns(boardId, boardBody.style.display);
         document.querySelector(`.toggle-btn-${boardId}`).firstElementChild.classList.remove("fas", "fa-chevron-down");
         document.querySelector(`.toggle-btn-${boardId}`).firstElementChild.classList.add("fas", "fa-chevron-up");
         boardBody.style.display = "flex";
     } else {
+        addColumns(boardId, boardBody.style.display);
         document.querySelector(`.toggle-btn-${boardId}`).firstElementChild.classList.remove("fas", "fa-chevron-up");
         document.querySelector(`.toggle-btn-${boardId}`).firstElementChild.classList.add("fas", "fa-chevron-down");
         boardBody.style.display = "none";
+    }
+}
+ function addColumns(boardId, style){
+    const toggleButtons = document.querySelectorAll('.board-toggle');
+    for (let button of toggleButtons) {
+        if (button.dataset.boardId === boardId){
+            let newColumnButton = document.createElement('button');
+            newColumnButton.textContent = 'Add new column';
+            let currentHeader = button.parentNode;
+            if (style === 'none'){
+                currentHeader.insertBefore(newColumnButton, button);
+            } else {
+                currentHeader.children[2].remove();
+            }
+
+        }
     }
 }
