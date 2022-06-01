8fdb811f88e7dfebf766430637bf2075c4d3d466
vbacso <bacsovanda95@gmail.com>
2022-06-01 09:55:45 +0200

    extend logSystem.js with login functions

--- static/js/controller/logSystem.js
+++ static/js/controller/logSystem.js
@@ -1,20 +1,19 @@
 import {dataHandler} from "../data/dataHandler.js";
 
-function createRegisterModal() {
+function createLogModal(title) {
     const modalContainer = document.createElement("div");
     modalContainer.innerHTML = `
         <div class="modal" tabindex="-1" id="new-board-modal">
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title">Register</h5>
+                <h5 class="modal-title">${title}</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body">
                     <label for="username">Username</label>
                     <input type="text" id="username" name="username" required>
-                    <br>
-                    <br>
+                    <br><br>
                     <label for="password">Password</label>
                     <input type="password" id="password" name="password" required>
               </div>
@@ -32,8 +31,8 @@ function createRegisterModal() {
     modal.show();
 }
 
-function getUserData(){
-    createRegisterModal();
+function registerUserData(){
+    createLogModal('Register');
     const saveBtn = document.querySelector('#save-new-user');
     saveBtn.addEventListener('click', () =>{
         const username = document.querySelector('#username').value;
@@ -45,7 +44,38 @@ function getUserData(){
 
 async function register(){
     const registerBtn = document.querySelector('#register-btn');
-    registerBtn.addEventListener('click', getUserData);
+    registerBtn.addEventListener('click', registerUserData);
+}
+
+function loginUserData(){
+    createLogModal('Login');
+    const saveBtn = document.querySelector('#save-new-user');
+    saveBtn.addEventListener('click', async () => {
+        const username = document.querySelector('#username').value;
+        const password = document.querySelector('#password').value;
+        const data = await dataHandler.loginUser(username, password);
+        if (typeof data === "object") {
+            alert(data[0])
+            createLoggedInDiv(data[1]);
+        }else{
+            alert(data)
+        }
+    });
+}
+
+function createLoggedInDiv(username){
+    const loggedInDiv = document.querySelector('.logged-in-msg');
+    const logInMsg = document.createElement('p')
+    logInMsg.classList.add('logged')
+    logInMsg.innerHTML = `Logged in as ${username}`
+    loggedInDiv.append(logInMsg)
+}
+
+
+async function login() {
+    const registerBtn = document.querySelector('#login-btn');
+    registerBtn.addEventListener('click', loginUserData);
 }
 
 register();
+login();
